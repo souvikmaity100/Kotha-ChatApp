0fc78fdf14b8349c1c6b42f6667000817d95f5e7
Souvik Maity <souvikmaity599@gmail.com>
2025-12-18 20:49:18 +0530

    fix: update CORS middleware to specify allowed origins and enable credentials

--- server/index.js
+++ server/index.js
@@ -39,7 +39,12 @@ io.on("connection", (socket) => {
 
 //  Middleware
 app.use(express.json({ limit: "10mb" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://chat.souvikmaity.space"],
+    credentials: true,
+  })
+);
 
 // Routes
 app.get("/", (req, res) => res.json({ message: "Server is Live" }));
